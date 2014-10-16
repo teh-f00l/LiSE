c96771787567aaf0f239a222c21390b7c06635e2
Zachary Spector <zacharyspector@gmail.com>
2014-10-15 23:06:00 -0400

    new way to report the status of the test

--- kobold.py
+++ kobold.py
@@ -287,26 +287,49 @@ def inittest(
         return thing['next_location'] is None
 
 
+def report_old(engine, n):
+    kobold_alive = 'kobold' in engine.character['physical'].thing
+    print(
+        "On tick {}, the dwarf is at {}, "
+        "and the kobold is {}{}{}".format(
+            n,
+            engine.character['dwarf'].avatar['location'],
+            "alive" if kobold_alive else "dead",
+            " at " + str(engine.character['kobold'].avatar['location'])
+            if kobold_alive else "",
+            " (in a shrubbery)" if kobold_alive and len([
+                th for th in
+                engine.character['kobold'].avatar.location.contents()
+                if th.name[:5] == "shrub"
+            ]) > 0 else ""
+        )
+    )
+
+
+def report(engine, n):
+    phys = engine.character['physical']
+    kobold_alive = 'kobold' in phys.thing
+    print(
+        "On tick {}, the dwarf is at {}, "
+        "and the kobold is {}{}{}".format(
+            n,
+            phys.thing['dwarf']['location'],
+            "alive" if kobold_alive else "dead",
+            " at " + str(phys.thing['kobold']['location'])
+            if kobold_alive else "",
+            " (in a shrubbery)" if kobold_alive and len([
+                th for th in phys.thing['kobold'].location.contents()
+                if th.name[:5] == "shrub"
+            ]) > 0 else ""
+        )
+    )
+
+
 def runtest(engine):
     # run sim for 100 tick
     for n in range(0, 100):
         engine.next_tick()
-        kobold_alive = 'kobold' in engine.character['physical'].thing
-        print(
-            "On tick {}, the dwarf is at {}, "
-            "and the kobold is {}{}{}".format(
-                n,
-                engine.character['dwarf'].avatar['location'],
-                "alive" if kobold_alive else "dead",
-                " at " + str(engine.character['kobold'].avatar['location'])
-                if kobold_alive else "",
-                " (in a shrubbery)" if kobold_alive and len([
-                    th for th in
-                    engine.character['kobold'].avatar.location.contents()
-                    if th.name[:5] == "shrub"
-                ]) > 0 else ""
-            )
-        )
+        report(engine, n)
 
 
 if __name__ == '__main__':
